6d26804172d01fe2b50ebac6a01b8c5c4b37803a
Mark O'Shea <mark.oshea@capitalone.com>
2015-08-25 19:40:45 +0100

    first working gem build
    now works as a gem executable

---
+++ lib/mobo.rb
@@ -0,0 +1,80 @@
+#!/usr/bin/env ruby
+require 'yaml'
+require 'pp'
+require 'logger'
+require_relative 'mobo/android'
+require_relative 'mobo/system_check'
+
+module Mobo
+  class << self
+    attr_accessor :log, :data, :devices
+
+    def log
+      @log || @log = Logger.new(STDOUT)
+    end
+
+    def cmd(command)
+      log.debug(command)
+      system(command)
+    end
+
+    def cmd_out(command)
+      log.debug(command)
+      `#{command}`
+    end
+
+    def start_process(cmd)
+      pid = Process.spawn(cmd)
+        Process.detach(pid)
+        pid
+      end
+
+    def devices
+      @devices = @devices.nil? ? {} : @devices
+    end
+
+    def device_config(device)
+      defaults = {
+        "name"        => "default",
+        "target"      => "android-22",
+        "abi"         => "default/x86_64",
+        "height"      => "1200",
+        "width"       => "720",
+        "sdcard_size" => "20M" }
+      defaults.merge(device)
+    end
+
+    def up
+      # retrive settings for one of each device
+      Mobo.data["devices"].each do |device|
+        device = device_config(device)
+        Mobo.devices[device["name"]] = device
+      end
+
+      # build and boot devices
+      Mobo.devices.each_pair do |name, device|
+        Mobo::Android::Avd.create(device)
+        emulator = Android::Emulator.new(device)
+        emulator.start
+        emulator.unlock_when_booted
+      end
+      File.open(MOBO_DEVICES_FILE, "w") do |file|
+        file.write Mobo.devices.to_yaml
+      end
+    end
+
+    def status
+      Mobo.data.each_pair do |name, device|
+        Mobo::Android::Emulator.new(device).status
+      end
+    end
+
+    def destroy
+      Mobo.data.each_pair do |name, device|
+        Mobo::Android::Emulator.new(device).destroy
+      end
+    end
+
+  end
+
+end
--- mobo.gemspec
+++ mobo.gemspec
@@ -1,11 +1,16 @@
+ruby_files = Dir.glob('*.rb') + Dir.glob('lib/**/*.rb') + Dir.glob('bin/**/*.rb')
+
+bin_files = ['bin/mobo']
+
 Gem::Specification.new do |s|
   s.name        = 'mobo'
   s.version     = '0.0.0'
   s.date        = '2015-08-05'
-  s.summary     = "Mobo - android emulator abstraction"
-  s.description = "Abstract android emaulators with a simple yaml file"
+  s.summary     = 'Mobo - android emulator abstraction'
+  s.description = 'Abstract android emaulators with a simple yaml file'
   s.authors     = ["Mark O'Shea"]
   s.email       = 'mark@osheatech.com'
-  s.files       = ["lib/mobo.rb", "lib/android.rb"]
-  s.homepage    ='http://osheatech.com'
+  s.executables = ['mobo']
+  s.files       = ruby_files + bin_files
+  s.homepage    = 'https://github.com/moshea/mobo'
 end
